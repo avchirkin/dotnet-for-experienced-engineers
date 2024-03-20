eb155f0c927077957417b3781423c0cfe59fbd1b
Алексей Чиркин <a4irkin@yandex.ru>
2024-03-20 23:23:22 +0300

    wip - part 1

---
+++ 2. Execution Flow. Operators and cycles/Lesson2/ConditionalOperators/Program.cs
@@ -0,0 +1,44 @@
+﻿// Выполняем код в блоке фигурных скобок под if, если выражение в круглых скобках равно true.
+// Оператор равенства == не стоит путать с оператором присвоения =.
+if (args.Length == 0)
+{
+    Console.WriteLine("Не передано параметров при запуске приложения");
+}
+
+// Запрашиваем ввод значения пользователем через консоль
+Console.WriteLine("Please, enter a number");
+var input = Console.ReadLine();
+
+// Пробуем парсить введённое значение как число типа int.
+// Если удалось распарсить, метод TryParse вернёт true, а результат парсинга сохранится в переменную number.
+if (int.TryParse(input, out var number))
+{
+    Console.WriteLine($"Input has successfully parsed as {number}");
+}
+else // если число распарсить не удалось (TryParse вернул false), попадём в эту ветку
+{
+    Console.WriteLine("Input is not a valid number");
+}
+
+Console.WriteLine("Please, enter a FizzBuzz number");
+
+// Пользуемся более "опасной" версией метода парсинга числа из строки - int.Parse. В отличии от TryParse,
+// этот метод вернет результат парсинга, если введено корректное число, либо упадёт с исключением в обратном случае.
+var fizzBuzzValue = int.Parse(Console.ReadLine() ?? string.Empty);
+if (fizzBuzzValue % 15 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+}
+else if (fizzBuzzValue % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if(fizzBuzzValue % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else
+{
+    Console.WriteLine("Not an interesting number");
+}
+
---
+++ 2. Execution Flow. Operators and cycles/Lesson2/Cycles/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит несколько чисел, разделённых пробелом
+Console.WriteLine("Enter several words splitted with whitespace (e.g., 'hello group how are you')");
+var numbersString = Console.ReadLine() ?? string.Empty;
+
+// Разделяем строку по пробелам и создаём массив чисел
+var numbers = numbersString.Split();
+
+#region Цикл for
+/* цикл for служит для гибкой настройки граничных условий цикла и его шага
+ *      int i = 0 - инициализация счётчика. Обязательная часть
+ *      i < numbers.Length - условие цикла - пока оно выполняется (равно true), цикл будет выполнять
+ * новую итерацию. Обязательная часть
+ *      i++ - инкремент счётчика. Необязательная часть (счётчик можно наращивать в теле цикла)
+ * 
+*/
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+// Выведем каждую третью букву слова.
+// Для этого установим начальное значение счётчика в 2 вместо 0, а шаг будем наращивать сразу на 3
+var word = "Somnambula";
+for (var i = 2; i < word.Length; i += 3)
+{
+    Console.Write(word[i]);
+}
+#endregion
+
+#region Цикл while
+#endregion
+
+#region Цикл do-while
+#endregion
+
+#region Цикл foreach
+#endregion
