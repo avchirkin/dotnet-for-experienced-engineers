839a792f4d2fd6d926ade6940b99cc46109dade4
Алексей Чиркин <a4irkin@yandex.ru>
2024-04-09 21:34:07 +0300

    Lesson 3 - added DynamicArray example

---
+++ 3. Arrays/Lesson3/DynamicArrayExample/DynamicArray.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace DenamicArrayProject
+{
+    internal class DynamicArray<T> : IEnumerable<T>
+    {
+        private T[] _items = new T[8];
+
+        private int _lastIndex = 0;
+
+        public void Add(T item)
+        {
+            int nextIndex = _lastIndex + 1;
+
+            if (nextIndex == _items.Length)
+            {
+                T[] resizedArr = new T[_items.Length * 2];
+                Array.Copy(_items, resizedArr, _items.Length);
+                _items = resizedArr;
+            }
+
+            _lastIndex = nextIndex;
+            _items[_lastIndex] = item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_items).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+}
