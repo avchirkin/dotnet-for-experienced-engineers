3ecf651904c5960fb505b428abd00ff40499dd60
achirkin <chirkin@dotnet.ru>
2024-05-05 16:01:19 +0300

    Lesson 10 - updates

--- 10. Strings/Lesson10/StringsEquality/Program.cs
+++ 10. Strings/Lesson10/StringsEquality/Program.cs
@@ -14,3 +14,17 @@ void ChangeString(string sourceString)
 {
     sourceString += " changed";
 }
+
+var strFour = "str";
+var strFive = "\nstr";
+var strSix = "\nstr";
+
+Console.WriteLine(strFour == strFive); // false
+Console.WriteLine(strFour.Equals(strFive, StringComparison.Ordinal)); // false
+Console.WriteLine(string.Compare(strFour, strFive, StringComparison.OrdinalIgnoreCase) == 0); // false
+Console.WriteLine(strFour.CompareTo(strFive) == 0); // false
+
+Console.WriteLine(strFive == strSix); // true
+Console.WriteLine(strFive.Equals(strSix, StringComparison.Ordinal)); // true
+Console.WriteLine(string.Compare(strFive, strSix, StringComparison.OrdinalIgnoreCase) == 0); // true
+Console.WriteLine(strFive.CompareTo(strSix) == 0); // true
